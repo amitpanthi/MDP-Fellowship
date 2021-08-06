05ed13933a46cd3a6db17f5557f7cf425a04df39
amit <apapanthi@gmail.com>
2021-08-06 12:05:55 +0530

    added r1q6

---
+++ day 2/review problems/temp_converter.js
@@ -0,0 +1,8 @@
+// Create a function that will convert from Celsius to Fahrenheit
+
+function convertCtoF(temperature){
+    return (temperature * 9/5) + 32
+}
+
+console.log(convertCtoF(0))
+console.log(convertCtoF(24))
