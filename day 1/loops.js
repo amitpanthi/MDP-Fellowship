3345a38469ffa953cb7c4fb4c85c5eff4d410b38
amit <apapanthi@gmail.com>
2021-08-06 09:16:14 +0530

    loops demo

---
+++ day 1/loops.js
@@ -0,0 +1,12 @@
+let anInt = 0
+
+console.log("While Loop")
+while (anInt < 5){
+    console.log(anInt)
+    anInt++
+}
+
+console.log("For Loop")
+for(let iter = 0; iter < 10; iter++){
+    console.log(iter*2)
+}
