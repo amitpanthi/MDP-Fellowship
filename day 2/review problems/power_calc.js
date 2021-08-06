5f90117d9de50d759de2b9fd67ea530f24fb605f
amit <apapanthi@gmail.com>
2021-08-06 15:41:18 +0530

    added r1q12

---
+++ day 2/review problems/power_calc.js
@@ -0,0 +1,8 @@
+// Create a function that takes voltage and current and returns the calculated power.
+// example : circuitPower(230, 10) ➞ 2300
+function calcPower(voltage, current){
+    return voltage*current
+}
+
+console.log(calcPower(230, 10))
+console.log(calcPower(100, 20))
