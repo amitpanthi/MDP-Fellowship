3f2f7fe89bb3cea25f9912f65415ca3e42e457b9
amit <apapanthi@gmail.com>
2021-08-06 10:14:12 +0530

    added day 2

---
+++ day 2/object_demo.js
@@ -0,0 +1,9 @@
+var person = {
+    name: "john",
+    age: 45,
+    occupation: "CA"
+}
+
+console.log(person)
+console.log(person.age)
+console.log(person["occupation"])
---
+++ day 2/scopes.js
@@ -0,0 +1,14 @@
+var f_name = "john"
+var f_name = "jonh"
+
+console.log(f_name)
+
+{
+    let anInt = 0
+    console.log(anInt)
+}
+
+//console.log(anInt) won't work
+
+const PI = 3.14
+//PI = 3.1415 won't work
