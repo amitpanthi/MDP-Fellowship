d3df1334dd12bf4807e16b3ac77516b253d1138b
amit <apapanthi@gmail.com>
2021-08-06 10:12:47 +0530

    added employee wage driver

---
+++ projects/employee wage/greeting.js
@@ -0,0 +1,12 @@
+// UC1 - Greeting
+console.log("Welcome to Employee Wage.")
+
+// UC2 - Attendance
+const IS_ABSENT = 0
+let currEmpAttendance = Math.floor(Math.random() * 10) % 2 //0 or 1 
+
+if (currEmpAttendance == IS_ABSENT){
+    console.log("Employee is Absent")
+} else {
+    console.log("Employee is Present")
+}
