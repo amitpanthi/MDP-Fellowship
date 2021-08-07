fb831b1a23d9b47821fe15c61a4ce4fb12e042e3
amit <apapanthi@gmail.com>
2021-08-07 15:22:30 +0530

    added for-loop variants in day 3/loops.js

--- day 3/loops.js
+++ day 3/loops.js
@@ -3,3 +3,16 @@ person = {
     lname: "Tron"
 }
 
+numbers = [11, 22, 33, 44, 55, 66, 77]
+
+for(const object_key in person){
+    console.log(person[object_key])
+}
+
+for(const object_key in numbers){
+    console.log(object_key)
+}
+
+for(const object_val of numbers){
+    console.log(object_val)
+}
