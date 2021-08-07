3fed99a945254438cec28fa606b2c80d634b53dd
amit <apapanthi@gmail.com>
2021-08-07 15:06:28 +0530

    added a function to find out wage based on conditions specified by max working hours

--- projects/employee wage/employee_wage.js
+++ projects/employee wage/employee_wage.js
@@ -56,32 +56,6 @@ function getDailyPay(workShift){
 }
 
 
-
-// UC3
-let wage = 0
-let workShift = Math.floor(Math.random() * 10) % 3
-
-function getDailyPay(workShift){
-    switch(workShift){
-        case 1: // Full Time
-            wage = constant_values.WAGE_PER_HR * constant_values.FULL_TIME
-            console.log("Wage for full time: " + wage)
-            break;
-        
-        case 0: // Part Time
-            wage = constant_values.WAGE_PER_HR * constant_values.PART_TIME
-            console.log("Wage for part time: " + wage)
-            break;
-        
-        default: // Absent
-            wage = 0
-            console.log("Wage when absent: " + wage)
-    }
-
-    return wage
-}
-
-
 // UC5 - Monthly Wage
 function getMonthlyWage(){
     let totalWage = 0
@@ -114,6 +88,53 @@ function getMonthlyWage(){
     }
 
     console.log("Total Wage:" + totalWage)
+    console.log("Days worked Full Time: " + fullTimeCounter)
+    console.log("Days worked Part Time: " + partTimeCounter)
+    console.log("Days absent: " + absentCounter)
+}
+
+
+// UC6 - Max Working Hours
+function maxWorkingHours(){
+    let totalHours = 0
+    let todayHours = 0
+    let fullTimeCounter = 0
+    let partTimeCounter = 0
+    let absentCounter = 0
+
+    for(var day=1; day < Number.MAX_VALUE; day++){
+        if(totalHours >= 160 || day >= 20){
+            break;
+        }
+
+        let currWorkShift = Math.floor(Math.random() * 10) % 3
+
+        switch(currWorkShift){
+            case 1: // Full Time
+                todayHours = constant_values.FULL_TIME
+                fullTimeCounter += 1
+                break;
+            
+            case 0: // Part Time
+                todayHours = constant_values.PART_TIME
+                partTimeCounter += 1
+                break;
+            
+            default:
+                todayHours = 0
+                absentCounter += 1
+                break;
+        }
+
+        totalHours += todayHours
+    }
+
+    totalWage = totalHours * constant_values.WAGE_PER_HR
+
+    console.log("--------------------------------")
+    console.log("Total Wage:" + totalWage)
+    console.log("Hours worked:" + totalHours)
+    console.log("Days passed: " + day)
     console.log("Days worked Full Time: " + fullTimeCounter)
     console.log("Days worked Part Time: " + partTimeCounter)
     console.log("Days absent: " + absentCounter)
