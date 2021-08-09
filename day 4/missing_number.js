42f3f3b8f9c43a9fc961c279aa61c59eca3d634f
amit <apapanthi@gmail.com>
2021-08-09 13:15:22 +0530

    added a function to find out a single missing number in an array of numbers from 1 to n

---
+++ day 4/missing_number.js
@@ -0,0 +1,18 @@
+// Find the missing number in a given integer array of 1 to 100? 
+
+let threeMissing = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+let ninetyNineMissing = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 100]
+
+function findMissingNum(array){
+    let upper_number = array.length + 1
+    let totalSum = upper_number * (upper_number + 1) / 2
+    let arraySum = 0
+
+    for(let iter = 0; iter < array.length; iter++){
+        arraySum += array[iter]
+    }
+    return totalSum - arraySum
+}
+
+console.log(findMissingNum(threeMissing))
+console.log(findMissingNum(ninetyNineMissing))
