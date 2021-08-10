62f2fa700136bcac3d72d41d472102b148cde3a9
amit <apapanthi@gmail.com>
2021-08-10 11:01:38 +0530

    updated EmployeePayroll class with setter functions for its attributes

--- projects/employee wage/employee_payroll.js
+++ projects/employee wage/employee_payroll.js
@@ -5,10 +5,24 @@ class EmployeePayroll{
         this.e_salary = salary
     }
 
+    setName(new_name){
+        this.e_name = new_name
+    }
+
+    setID(new_id){
+        this.e_id = new_id
+    }
+
+    setSalary(new_salary){
+        this.e_salary = new_salary
+    }
+
     showDetails(){
         console.log(`Employee ID: ${this.e_id}\nEmployee Name: ${this.e_name}\nEmployee Salary: ${this.e_salary}`)
     }
 }
 
 ramEmployee = new EmployeePayroll("E001242069", "Ram", 4200)
+ramEmployee.showDetails()
+ramEmployee.setSalary(6000)
 ramEmployee.showDetails()
