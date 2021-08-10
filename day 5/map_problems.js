0136ee7629750d938f6f89a4b815483bc8460b75
amit <apapanthi@gmail.com>
2021-08-10 12:09:22 +0530

    added a birthday simulation in map_problems, where we generate random values for months, and find out number of people who have birthdays in same month

--- day 5/map_problems.js
+++ day 5/map_problems.js
@@ -2,14 +2,12 @@
 class DiceSim{
     simulateDie(){
         let rollCounts = new Map()
+        let currRollFn = () => {
+            return Math.ceil(Math.random() * 6)
+        }
 
        for(let itr=0; ;itr++){
-            let currRollFn = () => {
-                return Math.ceil(Math.random() * 6)
-            }
-
             let currRoll = currRollFn()
-
             let currCount = rollCounts.get(currRoll) == undefined ? 1 : rollCounts.get(currRoll) + 1
             rollCounts.set(currRoll, currCount)
 
@@ -51,3 +49,27 @@ class DiceSim{
 ds = new DiceSim()
 countsdict = ds.simulateDie()
 ds.findMaxandMin(countsdict)
+
+// Birthday Sim
+class BirthdaySim{
+    generateBirthdays(){
+        let bdayCount = new Map();
+        let bdayGen = () => {
+            return Math.ceil(Math.random() * 12)
+        }
+
+        for(let person = 0; person < 50; person++){
+            let currPersonBday = bdayGen()
+            let currCount = bdayCount.get(currPersonBday) == undefined ? 1 : bdayCount.get(currPersonBday) + 1
+            bdayCount.set(currPersonBday, currCount)
+        }
+
+        bdayCount = new Map([...bdayCount].sort((a, b) => a[0] - b[0]))
+        for(let [month, count] of bdayCount){
+            console.log(`For Month ${month}: ${count}`)
+        }
+    }
+}
+
+bd = new BirthdaySim()
+bd.generateBirthdays()
