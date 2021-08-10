9d56d29ffd9e9f70c44252c9aa540b3580b5a19e
amit <apapanthi@gmail.com>
2021-08-10 13:49:26 +0530

    added a function to find out the second biggest and second smallest element in an array, present in array_problems.js

---
+++ day 5/array_problems.js
@@ -0,0 +1,38 @@
+// Find second largest and second smallest number
+function findSecondLargestAndSmallest(){
+    // Generate 10 Random 3 Digit number
+    let numberGen = () => {return Math.floor(Math.random() * (899) + 100)}
+    let randomNums = []
+
+    // Store this random numbers into a array
+    for(let i = 0; i < 10; i++){
+        randomNums.push(numberGen())
+    }
+
+    // Find the 2nd largest and the 2nd smallest element without sorting the array
+    let biggest = Number.MIN_VALUE
+    let secondBiggest = Number.MIN_VALUE
+    let smallest = Number.MAX_VALUE
+    let secondSmallest = Number.MAX_VALUE
+
+    for(const val of randomNums){
+        if(val >= biggest){
+            secondBiggest = biggest
+            biggest = val
+        } else if (val >= secondBiggest){
+            secondBiggest = val
+        }
+
+        if(val <= smallest){
+            secondSmallest = smallest
+            smallest = val
+        } else if (val <= secondSmallest){
+            secondSmallest = val
+        }
+    }
+
+    console.log(randomNums)
+    console.log(secondBiggest + " $ " + secondSmallest)
+}
+
+findSecondLargestAndSmallest()
