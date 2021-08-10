2eadfe04cccde642e3df65849a219b2580c4b622
amit <apapanthi@gmail.com>
2021-08-10 16:12:04 +0530

    added functions to day 5/array_problems.js which helps find triplets that add to zero inside an array, as well as another function to print 2 digit numbers like 33, 44, and so on upto a given upper_limit parameter

--- day 5/array_problems.js
+++ day 5/array_problems.js
@@ -41,3 +41,91 @@ function findSecondLargestAndSmallest(){
 }
 
 findSecondLargestAndSmallest()
+
+// Extend the Prime Factorization Program to store all the Prime Factors of a number n into an array and finally display the output.
+function displayPrimeFactors(number){
+    smallestPrimes = new Array(number + 1)
+
+    for(let iter=1; iter <= number; iter++){
+        smallestPrimes[iter] = iter
+        
+    }
+
+    for(let iter=4; iter<=number; iter+=2){
+        smallestPrimes[iter] = 2
+    }
+
+    for(let iter=3; iter*iter < number; iter++){
+        if(smallestPrimes[iter] == iter){
+            for(let j = iter*iter; j<number; j+=iter){
+                if(smallestPrimes[j] == j){
+                    smallestPrimes[j] = iter
+                }
+            }
+        }
+    }
+
+    let primeFactors = []
+    let ogNumber = number
+
+    while(number != 1){
+        primeFactors.push(smallestPrimes[number])
+        number = number/smallestPrimes[number]
+    }
+
+    console.log(`Prime Factors for ${ogNumber} are ${primeFactors.join(", ")}`)
+}
+
+displayPrimeFactors(200)
+
+
+// Program to show sum of three integer adds to ZERO
+function findTriplets(array){
+    let triplets = []
+    let foundValIdx = new Set()
+    
+    for(let i = 0; i < array.length; i++)
+        for(let j = 0; j < array.length; j++){
+            if(i == j || (foundValIdx.has(i) && foundValIdx.has(j))){
+                continue;
+            }
+
+            
+            if(array.find((num) => {return num == -1*(array[i] + array[j])})){
+                triplets.push([array[i], array[j], -1*(array[i] + array[j])])
+                foundValIdx.add(i)
+                foundValIdx.add(j)
+                foundValIdx.add(array.findIndex((num) => {return num == -1*(array[i] + array[j])}))
+            } 
+            
+        }
+
+    console.log(triplets)
+}
+
+let numbers = [-1, -2, 0, 3, 99, 100, -199]
+findTriplets(numbers)
+
+// Take a range from 0 – 100, find the digits that are repeated twice like 33, 77,etc and store them in an array
+function findSimilarNumbers(upper_limit){
+    let units_v = 1
+    let tens_v = 1
+    let similarNumbers = []
+    let currNum = 0
+
+    while(currNum < upper_limit){
+        currNum = tens_v*10 + units_v
+        if(currNum > upper_limit){
+            break;
+        }
+
+        similarNumbers.push(currNum)
+
+        units_v += 1
+        tens_v  += 1
+    }
+
+    console.log(similarNumbers)
+}
+
+findSimilarNumbers(100)
