6d25960c8be2fc74d9c9f1e422abe4c2925c925b
amit <apapanthi@gmail.com>
2021-08-10 11:23:20 +0530

    extended EmployeePayroll to include gender and startDate

--- projects/employee wage/employee_payroll.js
+++ projects/employee wage/employee_payroll.js
@@ -1,8 +1,26 @@
 class EmployeePayroll{
-    constructor(id, name, salary){
-        this.e_id = id
-        this.e_name = name
-        this.e_salary = salary
+    constructor(...params){
+        this.e_id = params[0]
+        this.e_name = params[1]
+        this.e_salary = params[2]
+        this.e_gender = params[3]
+        this.e_startDate = params[4]
+    }
+
+    setName(new_name){
+        this.e_name = new_name
+    }
+
+    setID(new_id){
+        this.e_id = new_id
+    }
+
+    setSalary(new_salary){
+        this.e_salary = new_salary
+    }
+
+    setGender(new_gender){
+        this.e_gender = new_gender
     }
 
     setName(new_name){
@@ -18,11 +36,12 @@ class EmployeePayroll{
     }
 
     showDetails(){
-        console.log(`Employee ID: ${this.e_id}\nEmployee Name: ${this.e_name}\nEmployee Salary: ${this.e_salary}`)
+        console.log(`Employee ID: ${this.e_id}\nEmployee Name: ${this.e_name}\nEmployee Salary: ${this.e_salary}\
+        \nGender: ${this.e_gender}\nStart Date: ${this.e_startDate}`)
     }
 }
 
-ramEmployee = new EmployeePayroll("E001242069", "Ram", 4200)
+ramEmployee = new EmployeePayroll("E001242069", "Ram", 4200, "Male", "23/10/2019")
 ramEmployee.showDetails()
 ramEmployee.setSalary(6000)
 ramEmployee.showDetails()
